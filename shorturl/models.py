1fe822d72fe7143d7ce32fe0f981026d7b075949
Farzana Noor <noorfarzana506@gmail.com>
2025-04-19 10:45:14 +0500

    create ShortURL model with fields

--- shorturl/models.py
+++ shorturl/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+import string, random
 
-# Create your models here.
+def generate_short_code(length=6):
+    return ''.join(random.choices(string.ascii_letters + string.digits, k=length)) #unique and random genrated
+
+class ShortURL(models.Model):
+    url = models.URLField()
+    short_code = models.CharField(max_length=10, unique=True, default=generate_short_code)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    access_count = models.IntegerField(default=0)
