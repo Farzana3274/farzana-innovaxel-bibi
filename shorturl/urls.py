147a9988fadc81d0adb3739a6d364e4611c361a3
Farzana Noor <noorfarzana506@gmail.com>
2025-04-19 11:14:46 +0500

    add GET view to retrieve short URL

--- shorturl/urls.py
+++ shorturl/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.ShortenURL.as_view()),
+    path('shorten/<str:code>', views.RetrieveURL.as_view()),
    
 ]
--- shorturl/views.py
+++ shorturl/views.py
@@ -13,3 +13,16 @@ class ShortenURL(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+#get url
+class RetrieveURL(APIView):
+    def get(self, request, code):
+        try:
+            short_url = ShortURL.objects.get(short_code=code)
+            short_url.access_count += 1
+            short_url.save()
+            serializer = ShortURLSerializer(short_url)
+            return Response(serializer.data)
+        except ShortURL.DoesNotExist:
+            return Response({'detail': 'Not found.'}, status=status.HTTP_404_NOT_FOUND)
