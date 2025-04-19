4a519f1be8cc477a24ac74dd0c08db44325743b7
Farzana Noor <noorfarzana506@gmail.com>
2025-04-19 18:25:08 +0500

    Add README only

--- shorturl/serializers.py
+++
@@ -1,7 +0,0 @@
-from rest_framework import serializers
-from .models import ShortURL
-
-class ShortURLSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = ShortURL
-        fields = '__all__'
